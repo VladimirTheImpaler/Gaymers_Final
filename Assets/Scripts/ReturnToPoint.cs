c7ed2032610b5f5a14279b1f76fb98cb29ad0a0b
Junkifii <69882147+Junkifii@users.noreply.github.com>
2023-03-06 17:10:30 -0600

    Reverted to Original JoeJeff in ReturnToPoint

--- Assets/Scripts/ReturnToPoint.cs
+++ Assets/Scripts/ReturnToPoint.cs
@@ -3,58 +3,61 @@ using System.Collections;
 using Valve.VR;
 using Valve.VR.InteractionSystem;
 
-public class ReturnToPoint : MonoBehaviour
+// NOTE: Take out all the irrelevant platformer stuff so it's just a return to point script
+// Implement distance calculator that returns to original spot once it's too far away
+
+namespace Valve.VR.InteractionSystem.Sample0
 {
-    // NOTE: Take out all the irrelevant platformer stuff so it's just a return to point script
-    // Implement distance calculator that returns to original spot once it's too far away
-
-    public Transform Joystick;
-    public float joyMove = 0.1f;
-
-    public SteamVR_Action_Vector2 moveAction = SteamVR_Input.GetAction<SteamVR_Action_Vector2>("platformer", "Move");
-    public SteamVR_Action_Boolean jumpAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("platformer", "Jump");
-
-    public JoeJeff character;
-
-    public Renderer jumpHighlight;
-
-    private Vector3 movement;
-    private bool jump;
-    private float glow;
-    private SteamVR_Input_Sources hand;
-    private Interactable interactable;
-
-    private void Start()
+    public class JoeJeffController : MonoBehaviour
     {
-        interactable = GetComponent<Interactable>();
-    }
+        public Transform Joystick;
+        public float joyMove = 0.1f;
 
-    private void Update()
-    {
-        if (interactable.attachedToHand)
-        {
-            hand = interactable.attachedToHand.handType;
-            Vector2 m = moveAction[hand].axis;
-            movement = new Vector3(m.x, 0, m.y);
+        public SteamVR_Action_Vector2 moveAction = SteamVR_Input.GetAction<SteamVR_Action_Vector2>("platformer", "Move");
+        public SteamVR_Action_Boolean jumpAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("platformer", "Jump");
 
-            jump = jumpAction[hand].stateDown;
-            glow = Mathf.Lerp(glow, jumpAction[hand].state ? 1.5f : 1.0f, Time.deltaTime * 20);
-        }
-        else
+        public JoeJeff character;
+
+        public Renderer jumpHighlight;
+
+        private Vector3 movement;
+        private bool jump;
+        private float glow;
+        private SteamVR_Input_Sources hand;
+        private Interactable interactable;
+
+        private void Start()
         {
-            movement = Vector2.zero;
-            jump = false;
-            glow = 0;
+            interactable = GetComponent<Interactable>();
         }
 
-        Joystick.localPosition = movement * joyMove;
+        private void Update()
+        {
+            if (interactable.attachedToHand)
+            {
+                hand = interactable.attachedToHand.handType;
+                Vector2 m = moveAction[hand].axis;
+                movement = new Vector3(m.x, 0, m.y);
 
-        float rot = transform.eulerAngles.y;
+                jump = jumpAction[hand].stateDown;
+                glow = Mathf.Lerp(glow, jumpAction[hand].state ? 1.5f : 1.0f, Time.deltaTime * 20);
+            }
+            else
+            {
+                movement = Vector2.zero;
+                jump = false;
+                glow = 0;
+            }
 
-        movement = Quaternion.AngleAxis(rot, Vector3.up) * movement;
+            Joystick.localPosition = movement * joyMove;
 
-        jumpHighlight.sharedMaterial.SetColor("_EmissionColor", Color.white * glow);
+            float rot = transform.eulerAngles.y;
 
-        character.Move(movement * 2, jump);
+            movement = Quaternion.AngleAxis(rot, Vector3.up) * movement;
+
+            jumpHighlight.sharedMaterial.SetColor("_EmissionColor", Color.white * glow);
+
+            character.Move(movement * 2, jump);
+        }
     }
 }

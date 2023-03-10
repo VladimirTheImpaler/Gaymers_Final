7293475f4869753c713cdacc9aa988cfd2914135
Mitch T <mtexel13@unl.edu>
2023-03-10 17:12:38 -0600

    Attempted Ball Spawning Script (Mitch Scene)

--- Assets/Scripts/tempBarrelScript.cs
+++ Assets/Scripts/tempBarrelScript.cs
@@ -5,25 +5,38 @@ using UnityEngine;
 public class tempBarrelScript : MonoBehaviour
 {
 
-    int timer = 100
+    public GameObject liquidDrop;
+
+    int timer = 1000;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        //liquidDrop.SetActive(true);
+        //Instantiate(liquidDrop, new Vector3(0, 5, 0), Quaternion.identity);
+    }
+
+    private void spawnBall()
+    {
+
+        GameObject a = Instantiate(liquidDrop, new Vector3(0, 5, 0), Quaternion.identity) as GameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer -= 1;
+
         
+
         if (timer < 0)
         {
 
-            timer = 100;
+            timer = 1000;
             //summon liquidBall
+            spawnBall();
         }
-
-
+        
     }
 }

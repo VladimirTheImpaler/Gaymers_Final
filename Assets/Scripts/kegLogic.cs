997ad607f9d3fd0e26e64dd4c9693d1a041df762
Mitch T <mtexel13@unl.edu>
2023-03-13 16:16:38 -0500

    Created Working Spigot button and cup (Mitch Scene)

--- Assets/Scripts/kegLogic.cs
+++ Assets/Scripts/kegLogic.cs
@@ -13,30 +13,28 @@ public class kegLogic : MonoBehaviour
     void Start()
     {
 
-        //liquidDrop.SetActive(true);
-        //Instantiate(liquidDrop, new Vector3(0, 5, 0), Quaternion.identity);
+
     }
 
-    private void spawnBall()
+    public void spawnBall()
     {
 
-        GameObject newLiquid = Instantiate(liquidDrop, new Vector3(-0.5f, 3.5f, 0.8f), Quaternion.identity) as GameObject;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         timer -= 1;
-
-        
 
         if (timer < 0)
         {
 
             timer = 100;
             //summon liquidBall
-            spawnBall();
+            GameObject newLiquid = Instantiate(liquidDrop, new Vector3(-0.5f, 3.5f, 0.8f), Quaternion.identity) as GameObject;
         }
         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
     }
 }

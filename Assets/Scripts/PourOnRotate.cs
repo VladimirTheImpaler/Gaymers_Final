83c67b734d0cdb949528ddbf5c514d5bcd3f57f8
Junkifii <69882147+Junkifii@users.noreply.github.com>
2023-03-22 16:06:54 -0500

    Functional PourOnRotate, applied to Bottle in MainScene

--- Assets/Scripts/PourOnRotate.cs
+++ Assets/Scripts/PourOnRotate.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PourOnRotate : MonoBehaviour
 {
+    public bool isPouring;
+    float pourCheckX;
+    float pourCheckZ;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        pourCheckX = GetComponent<RotationTracker>().rotationXYZ.x;
+        pourCheckZ = GetComponent<RotationTracker>().rotationXYZ.z;
+
+        if (((90.0f < pourCheckX) && (pourCheckX < 270.0f)) || ((90.0f < pourCheckZ) && (pourCheckZ < 270.0f))) 
+        {
+            isPouring = true;
+        }
+        else
+        {
+            isPouring = false;
+        }
     }
 }

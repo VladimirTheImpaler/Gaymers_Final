82112f213efd18180d4bb90d5db4bcb51f388b72
Mitch T <mtexel13@unl.edu>
2023-05-05 16:40:57 -0500

    Added Pipe to main scene, started tutorial book

---
+++ Assets/Scripts/bookScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bookScript : MonoBehaviour
+{
+
+    public GameObject helpText;
+
+    public bool inHand = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void showHelp()
+    {
+
+    }
+}
